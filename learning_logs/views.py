e796ef425fe75b7e23ab253cfcc6fd523f4c055a
DNA <dnamanh4@gmail.com>
2024-10-08 21:56:14 +0700

    2nd commit

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Topic,Entry
+from .form import TopicForm,EntryForm
 # Create your views here.
 
 def index(request):
@@ -17,3 +18,33 @@ def topic(request,topic_id):
     entries =topic.entry_set.order_by('-date_added')
     context = {'topic':topic,'entries':entries}
     return render(request, 'learning_logs/topic.html',context)
+
+def new_topic(request):
+    '''Them 1 topic'''
+    if request.method != 'POST':
+        #neu ko co data tra ve form rong
+        form = TopicForm()
+    else:
+        '''Xu ly data'''
+        form =TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    context = {'form':form}
+    return render(request,'learning_logs/new_topic.html',context)
+
+def new_entry(request,topic_id):
+
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic',topic_id = topic_id)
+    context ={'topic':topic,'form':form}
+    return render(request,'learning_logs/new_entry.html',context)
+
